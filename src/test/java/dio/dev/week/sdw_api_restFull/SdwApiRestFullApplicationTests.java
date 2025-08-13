30d81801273af38243d80c05b3b202a1a1faa9ed
Guilherme Alves <guilhermepontojesus@gmail.com>
2025-08-13 18:15:23 -0300

    Meu primeiro Commit

---
+++ src/test/java/dio/dev/week/sdw_api_restFull/SdwApiRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package dio.dev.week.sdw_api_restFull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SdwApiRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
